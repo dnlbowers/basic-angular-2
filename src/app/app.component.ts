f6e076be01dc9877995944e768dd9e016038f41f
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-03-31 00:06:44 +0200

    add pipe examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,15 @@ export class AppComponent {
 
   courses = COURSES;
 
-  // coreCourse = COURSES[0];
+  startDate = new Date(2000, 0, 1)
+
+  title = COURSES[0].description;
+
+  price = 9.99;
+
+  rate = 0.67;
+
+  course = COURSES[0];
 
   // rxjsCourse = COURSES[1];
   
