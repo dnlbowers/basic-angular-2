5b2bbe4282707442b37e0bb68570c42c2d0b193b
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-04-01 01:09:15 +0200

    add service and get and put call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { CoursesService } from './_service/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -17,22 +18,26 @@ export class AppComponent implements OnInit {
 
   // courses;
 
-  constructor(private http: HttpClient) {
+  constructor(private courseService: CoursesService) {
 
   }
 
   ngOnInit() {
 
-    const params = new HttpParams()
-      .set("page", "1")
-      .set("pageSize", "10");
-
-    this.courses$ = this.http.get<Course[]>('/api/courses', {params});
+    this.courses$ = this.courseService.loadCourses();
     
     
     // .subscribe(
     //   courses => this.courses = courses
     // )
+  }
+
+  save(course: Course) {
+    this.courseService.saveCourse(course)
+      .subscribe(
+        () => console.log('Course saved!')
+      );
+  
   }
 
 
